d242442e8663166450fd256ae0633deab61f96f8
bhawnaavaishnav <106766119+bhawnaavaishnav@users.noreply.github.com>
2022-08-02 11:13:27 +0530

    Second commit

---
+++ Portfolio.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    if (document.getElementById("profile").style.display == 'none') {
+        document.getElementById("profile").style.display = 'flex';
+    }
+    else {
+        document.getElementById("profile").style.display = 'none';
+    }
+}
